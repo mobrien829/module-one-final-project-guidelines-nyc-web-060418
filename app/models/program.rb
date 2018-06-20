4a764018d6cbcf56cd5afa8801af64b07c73ce27
github username <github email address>
2018-06-20 15:13:23 -0400

    added the info updater and show adder

--- app/models/program.rb
+++ app/models/program.rb
@@ -73,7 +73,12 @@ class Program
       id = Show.find_by(title: sanitize_user_input(show)).id
       self.user.dislike_show(id)
     when "3"
-      puts "Under Construction"
+      puts "Enter title of the show you want to update"
+      input = get_user_input
+      sani_input = sanitize_user_input(input)
+      selected_show = Show.where(title: sani_input).first
+      self.info_update_menu
+      self.info_update_selector(selected_show)
     when "4"
       puts "Here are other people with similar interests!"
       self.user.find_friends
@@ -84,9 +89,44 @@ class Program
     when "7"
       self.logout
       @onoff = 0
+    when "8"
+      puts "Enter show name here"
+      title_input = get_user_input
+      sani_title = sanitize_user_input(title_input)
+      Show.find_or_create_by(title: sani_title)
     end
 
   end
+
+
+      def info_update_selector(selected_show)
+        input = get_user_input
+        case input
+          when "1"
+            puts "Enter genre"
+            new_info = get_user_input
+            sani_info = sanitize_user_input(new_info)
+            selected_show.genre = sani_info
+            selected_show.save
+          when "2"
+            puts "Enter any additional details here"
+            new_info = get_user_input
+            selected_show.details = new_info
+            selected_show.save
+          when "3"
+            puts "Enter updated title"
+            new_title = get_user_input
+            sani_title = sanitize_user_input(new_title)
+            selected_show.title = sani_title
+            selected_show.save
+        end
+      end
+
+          def info_update_menu
+            puts "1 - Add/update genre"
+            puts "2 - Add/update details"
+            puts "3 - Add/update title"
+          end
 
   # private
 
